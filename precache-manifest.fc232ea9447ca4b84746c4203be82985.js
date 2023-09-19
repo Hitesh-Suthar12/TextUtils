561ff12d2c3a5d6b0f95ed3cb64a08692d997860
Nirmal Suthar <nirmalsuthar2008@gmail.com>
2023-09-20 00:10:22 +0530

    Updates

---
+++ precache-manifest.fc232ea9447ca4b84746c4203be82985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "18be5db4e089aef29a13",
+    "url": "/textutils/static/css/main.7632de33.chunk.css"
+  },
+  {
+    "revision": "18be5db4e089aef29a13",
+    "url": "/textutils/static/js/main.5e2c2c98.chunk.js"
+  },
+  {
+    "revision": "9eff2318fe253be3dc05",
+    "url": "/textutils/static/js/runtime~main.43961880.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/textutils/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/textutils/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "b7c5cef91e566b89938a7f3f032f2867",
+    "url": "/textutils/index.html"
+  }
+];
